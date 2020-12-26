b3f0a34840ab7411039ec1ae0a577a839e97344b
unknown <luoyifan1211@163.com>
2020-12-26 11:11:37 +0800

    init repo

---
+++ src/test/java/amazing/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package amazing.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
