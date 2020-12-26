977149dbcea22955917dfd6bdcdb99251863ffb8
unknown <luoyifan1211@163.com>
2020-12-26 20:52:46 +0800

    add cookies

--- src/main/java/amazing/community/controller/IndexController.java
+++ src/main/java/amazing/community/controller/IndexController.java
@@ -1,18 +1,40 @@
 package amazing.community.controller;
 
+import amazing.community.mapper.UserMapper;
+import amazing.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 //允许这个类去接收前端的请求
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String Index()
+    public String Index(HttpServletRequest request)
     {
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies)
+        {
+            if(cookie.getName().equals("token"))
+            {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user!=null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
